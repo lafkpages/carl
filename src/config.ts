49dad93c778d4a58fa9b10397006c45bed7a0788
lafkpages <luisafk@proton.me>
2024-07-09 17:23:52 +0200

    feat: config schema

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export interface Config {
+  $schema?: string;
+
+  /**
+   * User IDs that should be given certain permissions.
+   */
+  whitelist: {
+    admin: string[];
+    trusted: string[];
+  };
+
+  /**
+   * How many seconds to wait before allowing a user to run a command again.
+   */
+  ratelimit: {
+    admin: number;
+    trusted: number;
+  };
+}
