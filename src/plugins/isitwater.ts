f42d54e5760db792c38e6e2748b2ca4365557678
lafkpages <luisafk@proton.me>
2024-08-05 18:58:07 +0200

    feat: IsItWater react to location messages automatically

--- src/plugins/isitwater.ts
+++ src/plugins/isitwater.ts
@@ -1,4 +1,8 @@
+import type { ConsolaInstance } from "consola";
+import type { Message } from "whatsapp-web.js";
 import type { Plugin } from "../plugins";
+
+import assert from "node:assert";
 
 import { MessageTypes } from "whatsapp-web.js";
 
@@ -9,6 +13,47 @@ const apiKey = process.env.ISITWATER_API_KEY;
 
 if (!apiKey) {
   throw new Error("$ISITWATER_API_KEY is not set");
+}
+
+async function handleMessage(
+  message: Message,
+  logger: ConsolaInstance,
+  latitude?: string,
+  longitude?: string,
+) {
+  assert(apiKey);
+
+  if (!latitude) {
+    latitude = message.location.latitude;
+  }
+  if (!longitude) {
+    longitude = message.location.longitude;
+  }
+
+  assert(latitude);
+  assert(longitude);
+
+  const resp = await fetch(
+    `https://isitwater-com.p.rapidapi.com/?latitude=${encodeURIComponent(latitude)}&longitude=${encodeURIComponent(longitude)}&rapidapi-key=${encodeURIComponent(apiKey)}`,
+  );
+
+  if (!resp.ok) {
+    logger.error(
+      "Failed to fetch IsItWater API:",
+      resp.status,
+      resp.statusText,
+    );
+    return;
+  }
+
+  const { water } = await resp.json();
+
+  if (typeof water !== "boolean") {
+    logger.error("Invalid response from IsItWater API:", typeof water, water);
+    return;
+  }
+
+  await message.react(water ? "\u{1F30A}" : "\u26F0\uFE0F");
 }
 
 export default {
@@ -25,18 +70,17 @@ export default {
       rateLimit: 10000,
 
       async handler({ message, logger, rest }) {
+        let locationMessage = message;
         let latitude = "";
         let longitude = "";
 
         if (message.type === MessageTypes.LOCATION) {
-          latitude = message.location.latitude;
-          longitude = message.location.longitude;
+          locationMessage = message;
         } else if (message.hasQuotedMsg) {
           const quotedMsg = await message.getQuotedMessage();
 
           if (quotedMsg.type === MessageTypes.LOCATION) {
-            latitude = quotedMsg.location.latitude;
-            longitude = quotedMsg.location.longitude;
+            locationMessage = quotedMsg;
           }
         } else {
           const [, latArg, lngArg] = rest.match(/^\s*(.+?)[\s,]+(.+?)$/) ?? [];
@@ -50,32 +94,14 @@ export default {
           );
         }
 
-        const resp = await fetch(
-          `https://isitwater-com.p.rapidapi.com/?latitude=${encodeURIComponent(latitude)}&longitude=${encodeURIComponent(longitude)}&rapidapi-key=${encodeURIComponent(apiKey)}`,
-        );
-
-        if (!resp.ok) {
-          logger.error(
-            "Failed to fetch IsItWater API:",
-            resp.status,
-            resp.statusText,
-          );
-          return;
-        }
-
-        const { water } = await resp.json();
-
-        if (typeof water !== "boolean") {
-          logger.error(
-            "Invalid response from IsItWater API:",
-            typeof water,
-            water,
-          );
-          return;
-        }
-
-        await message.react(water ? "\u{1F30A}" : "\u26F0\uFE0F");
+        await handleMessage(locationMessage, logger, latitude, longitude);
       },
     },
   ],
+
+  async onMessage({ message, logger }) {
+    if (message.type === MessageTypes.LOCATION) {
+      await handleMessage(message, logger);
+    }
+  },
 } satisfies Plugin;
