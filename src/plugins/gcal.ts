dc20457284bb8a464508f424cd0ba6f02b088b15
lafkpages <luisafk@proton.me>
2024-08-22 01:35:11 +0900

    feat: /gcalevents

--- src/plugins/gcal.ts
+++ src/plugins/gcal.ts
@@ -1,8 +1,18 @@
-import type { Plugin } from "../plugins";
+import type { Interaction, Plugin } from "../plugins";
 
 import { google } from "googleapis";
 
+import { CommandError } from "../error";
 import { PermissionLevel } from "../perms";
+
+async function cal(
+  getGoogleClient: Parameters<Interaction["handler"]>[0]["getGoogleClient"],
+) {
+  const client = await getGoogleClient(
+    "https://www.googleapis.com/auth/calendar.readonly",
+  );
+  return google.calendar({ version: "v3", auth: client });
+}
 
 export default {
   id: "gcal",
@@ -18,11 +28,7 @@ export default {
       rateLimit: 10000,
 
       async handler({ getGoogleClient }) {
-        const client = await getGoogleClient(
-          "https://www.googleapis.com/auth/calendar.readonly",
-        );
-        const calendar = google.calendar({ version: "v3", auth: client });
-
+        const calendar = await cal(getGoogleClient);
         const calendars = await calendar.calendarList.list();
 
         if (!calendars.data.items) {
@@ -31,7 +37,36 @@ export default {
 
         let msg = "Calendars:";
         for (const cal of calendars.data.items) {
-          msg += `\n* ${cal.summary}`;
+          msg += `\n* \`${cal.id}\`: ${cal.summary}`;
+        }
+
+        return msg;
+      },
+    },
+    {
+      name: "gcalevents",
+      description: "List Google Calendar events",
+      minLevel: PermissionLevel.NONE,
+      rateLimit: 10000,
+
+      async handler({ rest, getGoogleClient }) {
+        const calendar = await cal(getGoogleClient);
+
+        if (!rest) {
+          throw new CommandError(
+            "missing calendar ID. Use `/gcalendars` to get a list of calendars.",
+          );
+        }
+
+        const events = await calendar.events.list({ calendarId: rest });
+
+        if (!events.data.items) {
+          return "No events found.";
+        }
+
+        let msg = "Events:";
+        for (const event of events.data.items) {
+          msg += `\n* ${event.summary}`;
         }
 
         return msg;
