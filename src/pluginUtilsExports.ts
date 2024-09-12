b83d37e6e98b931af19d1cc68e5f17195ec5969f
lafkpages <luisafk@proton.me>
2024-09-12 20:53:17 +0200

    refactor: remove now unnecessary pluginUtilsExports

--- src/pluginUtilsExports.ts
+++
@@ -1,6 +0,0 @@
-export type * from "./plugins";
-export { InteractionContinuation } from "./plugins";
-
-export { PermissionLevel } from "./perms";
-
-export { CommandError, CommandPermissionError } from "./error";
